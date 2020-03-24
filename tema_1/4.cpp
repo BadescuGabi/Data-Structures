55f5ffac4c361784af0b41bd094acf5399c460df
GabriBb4k <56481005+GabriBb4k@users.noreply.github.com>
2020-03-24 15:33:25 +0200

    Update 4.cpp

--- tema_1/4.cpp
+++ tema_1/4.cpp
@@ -6,46 +6,58 @@ using namespace std;
 const int di[] = {-1, 0, 1, 0},
         dj[] = {0, 1, 0, -1};
 int a[8][8], n, istart, jstart, i, j, k, ok = 0;
-
+int mini=999;
 void Fill(int istart, int jstart, int &v) {
-    int min = 999;
     ok = 0;
     queue<pair<int, int>> coada;
     coada.push(make_pair(istart, jstart));
-    a[istart][jstart] = v;
     i = coada.front().first;
     j = coada.front().second;
+    for(k = 0; k<4;k++){
+        int ivecin = i + di[k];
+        int jvecin = j + dj[k];
+        if(a[ivecin][jvecin]>mini and a[ivecin][jvecin]>1)
+            mini=a[ivecin][jvecin];
+    }
     for (k = 0; k < 4; k++) {
         int ivecin = i + di[k];
         int jvecin = j + dj[k];
-        if (a[ivecin][jvecin] < min && a[ivecin][jvecin] > 1) {
-            min = a[ivecin][jvecin];
+        if(a[ivecin][jvecin]==1 && a[istart][jstart]>1){
+            a[ivecin][jvecin]=a[istart][jstart];
         }
-        if (a[ivecin][jvecin] > 1) {
-            a[i][j] = min;
-        } else if (ivecin >= 0 && ivecin < n && jvecin >= 0 && jvecin < n && a[ivecin][jvecin] == 1) {
-            a[ivecin][jvecin] = v;
-            ok = 1;
-        }
-    }
-    if (ok == 0) {
-        v++;
-    }
-    coada.pop();
+        if (a[ivecin][jvecin] > 1 and a[istart][jstart]<a[ivecin][jvecin]) {
+            a[istart][jstart] = a[ivecin][jvecin];
+            ok=1;
+        }}
+if (a[istart][jstart]!=1){
+    return;
 }
+    if (ok == 0) {
+        a[istart][jstart] = ++v;
+        for (k = 0; k < 4; k++) {
+            int ivecin = i + di[k];
+            int jvecin = j + dj[k];
+            if(mini!=999 and a[ivecin][jvecin]==1)
+                a[ivecin][jvecin]=mini;
+            else if (a[ivecin][jvecin] == 1 && a[istart][jstart] > 1) {
+                a[ivecin][jvecin] = a[istart][jstart];
+            }}}
+            coada.pop();
+        }
 
 int main() {
     queue<pair<int, int>> coada;
-    int v = 2;
+    int v = 1;
     cin >> n;
     for (int i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
             cin >> a[i][j];
         }
     }
+    cout<<endl;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (a[i][j] != 0) {
+            if (a[i][j] >= 1) {
                 Fill(i, j, v);
             }
         }
